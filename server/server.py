2db0e88f8cdbb97e7a56562aaea3c6cf73f19986
ubaysubhan <ubaysubhan17@gmail.com>
2022-06-16 04:24:46 +0700

    chore: use queue

--- server/server.py
+++ server/server.py
@@ -14,6 +14,7 @@ game = {
 	"data": [],
 	"player_data": [],
 	"state": "roll",
+	"player_data_queue": [queue.Queue(), queue.Queue(), queue.Queue(), queue.Queue()]
 }
 command_queue = queue.Queue()
 game_lock = threading.Lock()
@@ -45,8 +46,13 @@ class ProcessTheClient(asyncore.dispatcher_with_send):
 		global game_lock
 		game_lock.acquire()
 		global game
+		print(rcv, ip)
+		index = game["player_data"].index(ip)
 		if rcv == "ask":
-			res = json.dumps({"status": "OK", "data": game["data"]})
+			data = []
+			while not game["player_data_queue"][index].Empty():
+				data = game["player_data_queue"][index].get()
+			res = json.dumps({"status": "OK", "data": data})
 		else:
 			command_queue.put(rcv)
 			res = json.dumps({"status": "OK", "data": ""})
@@ -66,10 +72,22 @@ def game_loop():
 				"ip": "server",
 				"action": "dice_{}".format(dice)
 			})
+			for i in range(4):
+				game["player_data_queue"][i].put({
+				"ip": "server",
+				"action": "dice_{}".format(dice)
+			})
 			game["state"] = "play"
 		elif game["state"] == "play":
+			game_lock.release()
 			cmd = command_queue.get()
+			game_lock.acquire()
 			game["data"].append({
+				"ip": cmd["ip"],
+				"action": cmd["action"]
+			})
+			for i in range(4):
+				game["player_data_queue"][i].put({
 				"ip": cmd["ip"],
 				"action": cmd["action"]
 			})
@@ -82,7 +100,7 @@ def game_loop():
 			})
 		game_lock.release()
 		time.sleep(5)
-		
+
 class Server(asyncore.dispatcher):
 	def __init__(self,portnumber):
 		asyncore.dispatcher.__init__(self)
